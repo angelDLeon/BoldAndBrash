6c2dbd5ddd7a52c8cb0d138a2e7840098fd27cd7
angelDLeon <angelica.deleon.736@my.csun.edu>
2021-11-01 20:30:34 -0700

    testing commit to branch only

--- BoldnBrash/add_listing.php
+++ BoldnBrash/add_listing.php
@@ -1,7 +1,7 @@
 <?php
   include('session.php');
 
-  // testing commit (Angelica)
+  // testing commit to branch only (Angelica)
   // check for submit
   if(isset($_POST['submit'])){
     $resource_name = mysqli_real_escape_string($conn, $_POST['resource_name']);
